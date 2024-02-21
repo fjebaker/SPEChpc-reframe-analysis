7aa8305763e1df93ba9dfeeadd9ab22baf917e29
fjebaker <fergusbkr@gmail.com>
2024-02-21 11:14:28 +0000

    feat: add optimal frequency scripts

---
+++ src/optimization.jl
@@ -0,0 +1,15 @@
+function find_optimal(target::TargetMetric, partition::Vector{RunInfo})
+    freqs = cpu_frequency.(partition)
+    unique_freqs, target_series = get_series_for(target.metric, freqs, partition)
+    series = mean.(target_series)
+    I = target.target_function.(series)
+
+    unique_freqs[I], series[I]
+end
+
+function filter_matching(target::TargetMetric, partition::Vector{RunInfo})
+    freqs, _ = find_optimal(target, partition)
+    filter(i -> any(i.cpu_frequency .≈ freqs), partition)
+end
+
+export find_optimal, filter_matching
